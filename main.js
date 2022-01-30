eb5517fca747356552f6474d71fe5fa045584c11
Stanley-Orabueze <67174967+Udonna-cell@users.noreply.github.com>
2022-01-30 13:38:08 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let data = document.querySelector('#url');
+let height = document.querySelector("#height");
+let width = document.querySelector("#height");
+let img = document.querySelector("[alt=QR]");
+let loading = document.querySelector("div.load");
+
+img.addEventListener("load",()=>{
+  loading.style.display = "none";
+  img.style.display = "inline";
+});
+
+data.addEventListener("input",()=>{
+  img.src = `http://api.qrserver.com/v1/create-qr-code/?data=${data.value}&color=fff&bgcolor=505CE5`;
+})
